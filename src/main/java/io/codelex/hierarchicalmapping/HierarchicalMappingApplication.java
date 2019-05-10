da4594b847d84caf6296f969ec0199b628aeeaa1
nils <kula.nils@gmail.com>
2019-05-10 23:19:09 +0300

    setup

---
+++ src/main/java/io/codelex/hierarchicalmapping/HierarchicalMappingApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.hierarchicalmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HierarchicalMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HierarchicalMappingApplication.class, args);
+	}
+
+}
